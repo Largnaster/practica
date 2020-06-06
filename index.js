66253f41e5632516c8e57e8e013ec7e1aa59e434
Largnaster <40041090+Largnaster@users.noreply.github.com>
2020-06-05 19:10:52 -0500

    Cambio en puertos

--- index.js
+++ index.js
@@ -37,27 +37,7 @@ app.use(history());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Configurar el puerto de escucha
-app.set("puerto", process.env.PORT || 3000);
+app.set("puerto", process.env.PORT || 8080);
 app.listen(app.get("puerto"), () => {
   console.log("Aplicacion escuchando por el puerto " + app.get("puerto"));
 });
-
-// const port = 3000
-
-// const MongoClient = require('mongodb').MongoClient
-
-// // Connection URL
-// const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
-
-// app.get('/', (req, res) => {
-//   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
-//     if (err) {
-//       res.status(500).send('NO SE HA PODIDO ESTABLECER CONEXIÓN!: ' + err);
-//     } else {
-//       res.send('Me conecté a la DB!');
-//       db.close();
-//     }
-//   });
-// });
-
-// app.listen(port, () => console.log(`Server listening on port ${port}!`))
